55d5f35c19ee240387f9d5c78acd899f9917ed7b
SaraG94 <sara.gherbassi.94@gmail.com>
2023-02-02 15:27:47 +0100

    ADD numbs 1-100

--- js/app.js
+++ js/app.js
@@ -1,4 +1,9 @@
 //serie di numeri da 1 a 100
-//multipli di 15 FizzBuzz
-//multipli di 5 Buzz
-//multipli di 3 Fizz
+for (let i =0; i<100; i++) {
+    console.log(i+1)
+    
+    //multipli di 15 FizzBuzz
+    //multipli di 5 Buzz
+    //multipli di 3 Fizz
+}
+
