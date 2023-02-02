a8273250604303b77340e2d8f06702c52b6dfb53
SaraG94 <sara.gherbassi.94@gmail.com>
2023-02-02 15:43:22 +0100

    FEAT multiples 5

--- js/app.js
+++ js/app.js
@@ -3,15 +3,18 @@ for (let i =0; i<100; i++) {
     console.log(i+1)
 
     // impostare resto multipli 15
-
     const restoMultiploQ = (i+1)% 15
+
     //Impostare resto multipli di 5
+    const restoMultiploCinque = (i+1)% 5
     
     //Iimpostare resto multipli di 3
 
-    //multipli di 15 FizzBuzz
+    //multipli di 15 FizzBuzz, multipli di 5 Buzz
     if (restoMultiploQ === 0){
         console.log('FizzBuzz')
+    }else if(restoMultiploCinque ===0){
+        console.log('Buzz')
     }
 
     
