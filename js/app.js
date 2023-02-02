c83bb8506620caa75b29e9842e1738bd2210074c
SaraG94 <sara.gherbassi.94@gmail.com>
2023-02-02 15:40:32 +0100

    FEAT multiples 15

--- js/app.js
+++ js/app.js
@@ -1,9 +1,21 @@
 //serie di numeri da 1 a 100
 for (let i =0; i<100; i++) {
     console.log(i+1)
+
+    // impostare resto multipli 15
+
+    const restoMultiploQ = (i+1)% 15
+    //Impostare resto multipli di 5
     
+    //Iimpostare resto multipli di 3
+
     //multipli di 15 FizzBuzz
-    //multipli di 5 Buzz
-    //multipli di 3 Fizz
+    if (restoMultiploQ === 0){
+        console.log('FizzBuzz')
+    }
+
+    
+    
+    
 }
 
