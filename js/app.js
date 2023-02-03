6c5662457efd121a92d896f6156efb797592df41
SaraG94 <sara.gherbassi.94@gmail.com>
2023-02-03 09:50:47 +0100

    FIX let num

--- js/app.js
+++ js/app.js
@@ -1,15 +1,16 @@
 //serie di numeri da 1 a 100
 for (let i =0; i<100; i++) {
-    console.log(i+1)
+    let num = i+1
+    console.log(num)
 
     // impostare resto multipli 15
-    const restoMultiploQ = (i+1)% 15
+    const restoMultiploQ = num % 15
 
     //Impostare resto multipli di 5
-    const restoMultiploCinque = (i+1)% 5
+    const restoMultiploCinque = num % 5
     
     //Iimpostare resto multipli di 3
-    const restoMultiploTre =(i+1)%3
+    const restoMultiploTre = num % 3
 
     //multipli di 15 FizzBuzz, multipli di 5 Buzz, multipli di 3 Fizz
     if (restoMultiploQ === 0){
